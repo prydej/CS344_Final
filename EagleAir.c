40fc190e4b33b8a89df3be572b9213a0bf87a33c
Julian Pryde <prydej@my.erau.edu>
2016-04-12 16:47:41 -0400

    "Write initial algorithm and start coding"  - started writing code to read in Daily file and create linked list at beginning of program

---
+++ EagleAir.c
@@ -0,0 +1,48 @@
+/*****************************************
+Title :	Eagle Airlines
+File	: EagleAir.c
+Date	: 12APR2016
+Author : Julian G. Pryde
+Course : CS344
+Section : 01
+Assignment : Final Project
+Input :	Group and Daily filename, chosen record
+Output : To terminal, records of work
+Method : linked lists, binary/text files
+*****************************************/
+
+#include <stdio.h>
+#include "action.h"
+
+//Declare Daily file linked list
+ListNodePtr startPtr;
+
+typedef struct listNodes {
+	int time;
+	int date;
+	int employee;
+	char[2] location;
+	char[50] description;
+}
+
+int main(void){
+	
+		
+	//open daily file
+	
+	//read in daily file to C linked list
+	
+	//close daily file
+	
+	//open group file
+	
+	//if group file exists, read in group file to C struct and add as node to linked list
+	
+	//ask user about displaying records
+	
+	//ask user for start/end date and time and employee #
+	
+	//display according records
+	
+	//loop the bitch
+}
---
+++ readDaily.c
@@ -0,0 +1,28 @@
+/*****************************************
+Title :	Eagle Airlines
+File	: readDaily.c
+Date	: 12APR2016
+Author : Julian G. Pryde
+Course : CS344
+Section : 01
+Assignment : Final Project
+Input :	Daily filename
+Output : Linked list with all information in daily file
+Method : printf function 
+*****************************************/
+
+#include <stdio.h>
+
+int main(char[256] filename, ListNodePtr startPtr){
+	
+	FILE * daily;
+	
+	//open daily file
+	daily = fopen(filename, "rb");
+	
+	//if daily file exists, fill linked list with daily file info
+	if (daily != NULL){
+		for 
+	}
+	
+}
